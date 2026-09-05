f17c4e0d3728b5c7fa18ec276fe5e5fa65577f52
dttdrv <dttdrv@users.noreply.github.com>
2026-09-05 03:47:17 +0300

    Preserve execution dependencies across Metal encoders

--- MoltenVK/MoltenVK/Commands/MVKCmdPipeline.mm
+++ MoltenVK/MoltenVK/Commands/MVKCmdPipeline.mm
@@ -104,6 +104,10 @@ VkResult MVKCmdPipelineBarrier<N>::setContent(MVKCommandBuffer* cmdBuff,
 	for (uint32_t i = 0; i < imageMemoryBarrierCount; i++) {
 		_barriers.emplace_back(pImageMemoryBarriers[i], srcStageMask, dstStageMask);
 	}
+	if (_barriers.empty()) {
+		VkMemoryBarrier barrier = {VK_STRUCTURE_TYPE_MEMORY_BARRIER};
+		_barriers.emplace_back(barrier, srcStageMask, dstStageMask);
+	}
 
 	return VK_SUCCESS;
 }
